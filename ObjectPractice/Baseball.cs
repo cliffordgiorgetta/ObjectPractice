8f4a543ac7078008bbfd8f97a2658bd3bad81cf8
SeedPaths-User <lessdismissive@email.com>
2014-04-25 13:42:57 -0600

    ObjectPractice
    objectpracticeonline

---
+++ ObjectPractice/Baseball.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObjectPractice
+{
+    class Baseball
+    {
+        public int glove;
+        public int bat;
+        public int baseball;
+        public int cleats;
+        public int shirt;
+        public int pants;
+
+
+        public class Teams
+        {
+            public int padres;
+            public int rockies;
+            public int braves;
+            public int cardinals;
+            public int nationals;
+        }
+    }
+}
---
+++ ObjectPractice/Food.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObjectPractice
+{
+    class Food
+    {
+        public int desert;
+        public int breakfast;
+        public int lunch;
+        public int dinner;
+
+        public class desert
+        {
+            public int icecream;
+            public int whipcream;
+            public int cake;
+            public int pie;
+        }
+    }
+}
